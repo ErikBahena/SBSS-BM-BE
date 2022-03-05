4826fc71ab64dee7c71b7429c160850202bbfd20
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-03-04 19:15:37 -0800

    user seed passwords are now hashed

--- data/seeds/01-user.js
+++ data/seeds/01-user.js
@@ -1,8 +1,17 @@
+const bcrypt = require("bcryptjs");
+const { BCRYPT_ROUNDS } = require("../../config");
+
+const hashPassword = (originalPassword) => {
+  return bcrypt.hashSync(originalPassword, BCRYPT_ROUNDS);
+};
+
 exports.seed = function (knex) {
   return knex("user").insert([
     {
       email: "test@gmail.com",
-      password: "test",
+      first_name: "fname",
+      last_name: "lname",
+      password: hashPassword("test"),
       user_id: 1,
     },
   ]);
