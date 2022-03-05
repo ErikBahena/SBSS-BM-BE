12715d5c7b82ad510511b32093eb62ea3f1dc508
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-03-04 19:59:32 -0800

    optimizing login auth endpoint

--- api/server.js
+++ api/server.js
@@ -14,7 +14,9 @@ server.use("/api/auth", authRouter);
 server.use("/api/user", userRouter);
 
 server.use((err, req, res, next) => {
-  res.status(err.status || 500).json({ message: err.message });
+  res
+    .status(err.status || 500)
+    .json({ message: err.message, stack: err.stack });
 });
 
 module.exports = server;
