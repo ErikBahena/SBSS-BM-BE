fca6b2a44ed20953149e4c7bec30e54c28248ba7
Erik Bahena <erikjbahena@gmail.com>
2022-03-30 23:20:33 -0700

    seeding prod issue

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT,
   JWT_SECRET: process.env.JWT_SECRET,
-  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS,
+  BCRYPT_ROUNDS: +process.env.BCRYPT_ROUNDS,
   NODE_ENV: process.env.NODE_ENV,
 };
