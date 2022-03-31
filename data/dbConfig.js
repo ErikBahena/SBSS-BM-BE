dabfaacdd77a8a6eb342509410aade27f2591dfe
Erik Bahena <erikjbahena@gmail.com>
2022-03-31 00:26:43 -0700

    updated and removed hash/compare sync in validation and pwhash funcs

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 const knexConfig = require("../knexfile");
 const { NODE_ENV } = require("../config");
 
-const environment = NODE_ENV || "development";
+const environment = NODE_ENV;
 
 module.exports = knex(knexConfig[environment]);
--- data/seeds/01-user.js
+++ data/seeds/01-user.js
@@ -1,12 +1,12 @@
 const { hashPassword } = require("../../api/auth/auth-helpers");
 
-exports.seed = function (knex) {
+exports.seed = async function (knex) {
   return knex("user").insert([
     {
       email: "guest@gmail.com",
       first_name: "guest",
       last_name: "account",
-      password: hashPassword("guestPassword!!221"),
+      password: await hashPassword("guestPassword!!221"),
       policy: true,
     },
   ]);
